1330090f40fdb89e96934fc95df24d5b4faa4241
s-woon <sus9763@gmail.com>
2023-05-24 23:06:03 +0900

    Fix: 엔티티 추가

---
+++ src/main/java/com/highfive/refurmoa/entity/Payment.java
@@ -0,0 +1,61 @@
+package com.highfive.refurmoa.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+public class Payment {
+
+    @Id
+    @Column(nullable = false)
+    private int payNum;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    @Column(length = 15)
+    private Member memberId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_num", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Board boardNum;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_code", nullable = false)
+    @OnDelete(action = OnDeleteAction.NO_ACTION)
+    private Product productCode;
+
+    @Column(name = "prod_price", nullable = false)
+    private int prodPrice;
+
+    @Column(name = "delivery_price", nullable = false)
+    private int deliveryPrice;
+
+    @Column(name = "pay_price", nullable = false)
+    private int payPrice;
+
+    @Column(name = "buy_method", nullable = false, length = 10)
+    private String buyMethod;
+
+    @Column(name = "coupon_num")
+    private int couponNum;
+
+    @Column(name = "mile_use")
+    private int mileUse;
+
+    @Column(name = "as_date")
+    private Date asDate;
+
+    @Column(name = "pay_date")
+    private Date payDate;
+
+    @Column(name = "pay_cancel", nullable = false)
+    private boolean payCancel;
+}
