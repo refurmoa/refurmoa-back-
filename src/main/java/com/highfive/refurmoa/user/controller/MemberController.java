c974d9794bc256e9acddff66928009ae20fd6ec6
chj <sajun28@naver.com>
2023-05-25 14:19:16 +0900

    signup    signup/distinct 합본

--- src/main/java/com/highfive/refurmoa/user/controller/MemberController.java
+++ src/main/java/com/highfive/refurmoa/user/controller/MemberController.java
@@ -1,11 +1,14 @@
 package com.highfive.refurmoa.user.controller;
 
-import com.highfive.refurmoa.user.dto.request.SignupRequestDto;
-import com.highfive.refurmoa.user.service.MemberServiceImpl;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.highfive.refurmoa.entity.Member;
+import com.highfive.refurmoa.user.dto.request.SignupRequestDto;
+import com.highfive.refurmoa.user.service.MemberServiceImpl;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,4 +20,18 @@ public class MemberController {
     public int insertMember(@RequestBody SignupRequestDto signupRequestDto) {
         return memberServiceImpl.insertMember(signupRequestDto);
     }
+    
+  //일반 쿼리문
+//	@RequestMapping("/signup/distinct")
+//	public int countMember(@RequestBody Member vo) {
+//		System.out.println(vo);
+//		return memberServiceImpl.countMember(vo.getMemberId());
+//	}
+	
+//	jpa repository 활용
+	@PostMapping("/signup/distinct")
+	public long countMemberId(@RequestBody Member vo) {
+		System.out.println(vo);
+		return memberServiceImpl.countMemberId(vo.getMemberId());
+	}
 }
