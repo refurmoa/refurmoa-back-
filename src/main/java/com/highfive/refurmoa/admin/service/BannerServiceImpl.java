baa1d9084b110517e93a1ec47b17fc7ee9a8bd0c
s-woon <sus9763@gmail.com>
2023-05-26 14:27:03 +0900

    Feat: 배너 목록 조회(페이징), 목록 검색(페이징), 삭제 기능 구현

--- src/main/java/com/highfive/refurmoa/admin/service/BannerServiceImpl.java
+++ src/main/java/com/highfive/refurmoa/admin/service/BannerServiceImpl.java
@@ -4,13 +4,14 @@ import com.highfive.refurmoa.admin.dto.request.WriteBannerRequestDTO;
 import com.highfive.refurmoa.admin.repository.BannerRepository;
 import com.highfive.refurmoa.entity.Banner;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -25,16 +26,19 @@ public class BannerServiceImpl implements BannerService {
         this.repository = repository;
     }
 
+    // 배너 목록 조회
     @Override
-    public Banner getBanner(int bannnum) {
-        return repository.findByBannNum(bannnum);
+    public Page<Banner> getListBanner(Pageable pageable) {
+        return repository.findAll(pageable);
     }
 
+    // 배너 목록 검색
     @Override
-    public List<Banner> getListBanner() {
-        return repository.findAll();
+    public Page<Banner> searchBanner(String search, Pageable pageable) {
+        return repository.findBySellerNameContaining(search, pageable);
     }
 
+    // 배너 등록
     @Override
     public int writeBanner(WriteBannerRequestDTO writeBannerRequestDTO, MultipartFile banner_img) throws IOException {
         String banner_image = saveImage(banner_img); // 이미지파일 이름
@@ -42,10 +46,18 @@ public class BannerServiceImpl implements BannerService {
         repository.save(banner);
         return 1;
     }
+    // 이미지 파일 이름 만들기
     private String saveImage(MultipartFile imageFile) throws IOException {
         String imgName = UUID.randomUUID() + "." +StringUtils.getFilename(imageFile.getOriginalFilename());
         File file = new File(imageDir + "banner\\" + imgName);
         imageFile.transferTo(file);
         return imgName;
+    }
+
+    // 배너 삭제
+    @Override
+    public int deleteBanner(int bann_num) {
+        repository.deleteByBannNum(bann_num);
+        return 1;
     }
 }
