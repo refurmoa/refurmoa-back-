4d037cf1903da4053db21a921c92b40eeed10fb4
s-woon <sus9763@gmail.com>
2023-05-27 22:54:29 +0900

    Fix: 주석 위치 수정

--- src/main/java/com/highfive/refurmoa/cs/service/FaqBoardService.java
+++ src/main/java/com/highfive/refurmoa/cs/service/FaqBoardService.java
@@ -8,19 +8,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface FaqBoardService {
-    // Faq 목록 조회
-    public Page<FaqBoard> getFaqBoard(int faq_cate, Pageable pageable);
-
-    // Faq 목록 검색(제목)
-    public Page<FaqBoard> searchFaqBoard(String search, Pageable pageable);
-
-    // Faq 삭제
-    public int deleteFaqBoard(@RequestParam int faq_num);
-
-    // Faq 작성
-    public int writeFaqBoard(FaqBoardWriteDTO faqBoardWriteDTO);
-
-    // Faq 수정
-    public int updateFaqBoard(FaqBoardUpdateDTO faqBoardUpdateDTO);
+    public Page<FaqBoard> getFaqBoard(int faq_cate, Pageable pageable); // Faq 목록 조회
+    public Page<FaqBoard> searchFaqBoard(String search, Pageable pageable); // Faq 목록 검색(제목)
+    public int deleteFaqBoard(@RequestParam int faq_num);// Faq 삭제
+    public int writeFaqBoard(FaqBoardWriteDTO faqBoardWriteDTO); // Faq 작성
+    public int updateFaqBoard(FaqBoardUpdateDTO faqBoardUpdateDTO); // Faq 수정
 
 }
