5316f412525a2fffd3623d6d92959132f3b7e799
s-woon <sus9763@gmail.com>
2023-05-31 02:45:15 +0900

    Feat: userlike 기능 구현

--- src/main/java/com/highfive/refurmoa/post/repository/UserlikeRepository.java
+++ src/main/java/com/highfive/refurmoa/post/repository/UserlikeRepository.java
@@ -1,8 +1,12 @@
 package com.highfive.refurmoa.post.repository;
 
 import com.highfive.refurmoa.entity.Userlike;
+import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserlikeRepository extends JpaRepository<Userlike, Integer> {
-    Userlike findByBoardNumAndMemberId(int boardNum, String memberId);
+    Userlike findByBoardBoardNumAndMemberMemberId(int board_num, String member_id);
+    List<Userlike> findByMemberMemberId(String memberId);
 }
