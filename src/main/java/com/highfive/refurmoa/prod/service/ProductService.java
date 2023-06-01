a6af3f23f3dfcc5e66286ec49ac21ea57b158dee
wstarY25 <127380218+wstarY25@users.noreply.github.com>
2023-06-01 12:26:44 +0900

    Update ProductService.java

--- src/main/java/com/highfive/refurmoa/prod/service/ProductService.java
+++ src/main/java/com/highfive/refurmoa/prod/service/ProductService.java
@@ -17,8 +17,8 @@ public interface ProductService {
 	
 	public List<ProdListDTO> productList(String category,String status);//상품 목록
 	public int productDelete(int code);//상품 삭제
-//	public int ProductWrite(MultipartFile mainImg,ProductWriteDTO prodDto) throws IOException;//상품 등록
-//	public int ProductUpdate(MultipartFile mainImg,ProductWriteDTO prodDto)throws IOException;// 상품 수정
+	public int ProductWrite(MultipartFile mainImg,ProductWriteDTO prodDto) throws IOException;//상품 등록
+	public int ProductUpdate(MultipartFile mainImg,ProductWriteDTO prodDto)throws IOException;// 상품 수정
 	public void insertFile(ProdFileDTO dto);//상품 이미지 저장
 	public ProdResponseDTO productInfo(int productCode);// 상품 정보 조회
 	public void insertProd(int comNum,Product prod);//상품 제휴회사 검색
