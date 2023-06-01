c3ae0fca539e34bb96e01d144f7037b909a9c162
ijh4u <ijh4u1@naver.com>
2023-06-02 00:48:05 +0900

    FIX: AS매장 List->Page

--- src/main/java/com/highfive/refurmoa/cs/repository/AsRepository.java
+++ src/main/java/com/highfive/refurmoa/cs/repository/AsRepository.java
@@ -2,6 +2,8 @@ package com.highfive.refurmoa.cs.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,11 +13,11 @@ import com.highfive.refurmoa.entity.AsStore;
 
 public interface AsRepository extends JpaRepository<AsStore, Integer> {
 
-		
-	@Query(value="select * from as_store where store_addr like %:storeAddr% and store_addr like :storeDetail%", nativeQuery=true)
-	List<AsStore> findByStoreAddrAndStoreDetail(@Param("storeAddr") String storeAddr, @Param("storeDetail") String storeDetail);	// as 매장 지역 검색
+	@Query("select p from AsStore p where p.storeAddr like %:storeAddr% and p.storeAddr like %:storeDetail%")
+	Page<AsStore> findByStoreAddrAndStoreDetail(@Param("storeAddr") String storeAddr, @Param("storeDetail") String storeDetail,Pageable pageable);	// as 매장 지역 검색
 	
-	List<AsStore> findByStoreNameContaining(String storeName);	// as 매장명 검색
+	Page<AsStore> findByStoreNameContaining(String storeName,Pageable pageable);	// as 매장명 검색
 	
-}
+	Page<AsStore> findAll(Pageable pageable);
 
+}
