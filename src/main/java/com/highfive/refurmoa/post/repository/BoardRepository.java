0be6139b2a381205056878fa84c5f725312b4cf1
s-woon <sus9763@gmail.com>
2023-06-02 14:21:55 +0900

    Refactor: import 수정

--- src/main/java/com/highfive/refurmoa/post/repository/BoardRepository.java
+++ src/main/java/com/highfive/refurmoa/post/repository/BoardRepository.java
@@ -1,24 +1,17 @@
 package com.highfive.refurmoa.post.repository;
 
 import com.highfive.refurmoa.entity.Board;
-import com.highfive.refurmoa.post.dto.PostReadCountResquestDTO;
 
 import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 
-import jakarta.transaction.Transactional;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Integer> {
