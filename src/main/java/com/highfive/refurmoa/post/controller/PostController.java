7d3ef63e8bd8076a16f552619b57891284b17832
s-woon <sus9763@gmail.com>
2023-06-02 14:20:36 +0900

    Refactor: DTO 위치 수정

--- src/main/java/com/highfive/refurmoa/post/controller/PostController.java
+++ src/main/java/com/highfive/refurmoa/post/controller/PostController.java
@@ -1,16 +1,12 @@
 package com.highfive.refurmoa.post.controller;
 
-import com.highfive.refurmoa.entity.Board;
-import com.highfive.refurmoa.post.dto.PostReadCountResquestDTO;
-import com.highfive.refurmoa.post.dto.PostRequestDTO;
-import com.highfive.refurmoa.post.dto.PostResponseDTO;
+import com.highfive.refurmoa.post.dto.request.PostReadCountResquestDTO;
+import com.highfive.refurmoa.post.dto.request.PostRequestDTO;
+import com.highfive.refurmoa.post.dto.reponse.PostResponseDTO;
 import com.highfive.refurmoa.post.dto.request.UserlikeRequestDTO;
 import com.highfive.refurmoa.post.service.PostServiceImpl;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
-
-import java.awt.print.Pageable;
-import java.util.List;
 
 @RestController
 @RequestMapping("/post")
