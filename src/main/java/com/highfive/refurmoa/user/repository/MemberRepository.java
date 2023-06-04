1412127d0ea9fd29ceccd21eb3b87d82f2b565e9
wstarY25 <w_star25@naver.com>
2023-06-04 15:32:00 +0900

    Refactor: 개인정보 수정 기능 파일 이동

--- src/main/java/com/highfive/refurmoa/user/repository/MemberRepository.java
+++ src/main/java/com/highfive/refurmoa/user/repository/MemberRepository.java
@@ -14,22 +14,14 @@ import com.highfive.refurmoa.user.DTO.reponse.AdminUserListResponseDTO;
 public interface MemberRepository extends JpaRepository<Member, String> {
 
 	Member findByMemberIdAndPassword(String member_id, String password);
-	
-	Member findByMemberId(String member_id);
 
 	Member findByNameAndPhone(String name, String phone);
 
 	Member findByMemberIdAndNameAndPhone(String member_id, String name, String phone);
 
-	long countByMemberId(String memberId);	    // ID 중복 검사
-	  
-	List<Member> findAllByMemberId(String memberId);	// 회원정보 불러오기
+	long countByMemberId(String memberId); // ID 중복 검사
 	
-	Member findAllByMemberIdAndAcceptLocation(String memberId, boolean acceptLocation);	//	회원주소검색
-	
-//	// admin 회원 목록 조회
-//	@Query(value="select m.MEMBER_ID, NAME, PHONE, GRADE, MILE, count(c.COUPON_NUM) as cnt from member m left join coupon c on m.MEMBER_ID = c.MEMBER_ID group by m.MEMBER_ID", nativeQuery=true)
-//	public List<AdminUserListResponseDTO> listAdminMember();
+	Member findAllByMemberIdAndAcceptLocation(String memberId, boolean acceptLocation);	// 회원 주소 검색
 	
 	// admin 회원 목록 조회
 	@Query(value="select m.MEMBER_ID, NAME, PHONE, GRADE, MILE, count(c.COUPON_NUM) as cnt from member m left join coupon c on m.MEMBER_ID = c.MEMBER_ID group by m.MEMBER_ID", nativeQuery=true)
@@ -38,8 +30,6 @@ public interface MemberRepository extends JpaRepository<Member, String> {
 	// admin 회원 검색
 	@Query(value="select m.MEMBER_ID, NAME, PHONE, GRADE, MILE, count(c.COUPON_NUM) as cnt from member m left join coupon c on m.MEMBER_ID = c.MEMBER_ID where m.MEMBER_ID like %:memberId% or NAME like %:memberId% group by m.MEMBER_ID", nativeQuery=true)
 	public List<AdminUserListResponseDTO> findByMemberIdOrName(@Param("memberId") String memberId);
-	
-	
-	
 
+	Member findByMemberId(String memberId); // 결제 - 회원 정보 조회
 }
--- src/main/java/com/highfive/refurmoa/user/service/MemberService.java
+++ src/main/java/com/highfive/refurmoa/user/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.highfive.refurmoa.user.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
@@ -15,9 +16,7 @@ public interface MemberService {
     public String findPW(String member_id, String name, String phone); // PW 찾기
     public void insertMember(Member member); // 회원가입
     public long countMemberId(String memberId); // ID 중복 검사
-    public List<Member> listMember(String memberId);	// 회원정보 불러오기
-    public void deleteMember(String memberId);	// 회원탈퇴
-    public Member updateMember(Member member);	// 회원정보 수정
+
     public String userLocationInfo(String memberId, boolean acceptLocation);	//	회원주소검색
 //    public List<AdminUserListResponseDTO> listAdminMember();	// 관리자 페이지 회원 목록 조회
     public Slice<AdminUserListResponseDTO> listAdminMember(Pageable pageable);	// 관리자 페이지 회원 목록 조회
