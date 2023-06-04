2092c536117d4773caf8ab6e4355f85d04467c05
ijh4u <ijh4u1@naver.com>
2023-06-04 23:31:34 +0900

    @NoArgsConstructor

--- src/main/java/com/highfive/refurmoa/entity/Mile.java
+++ src/main/java/com/highfive/refurmoa/entity/Mile.java
@@ -3,14 +3,16 @@ package com.highfive.refurmoa.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Mile {
 
     @Id
