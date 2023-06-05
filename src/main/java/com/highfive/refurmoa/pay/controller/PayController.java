ab0a168797211f1f099ab8901c1bac4b6c8740ae
ijh4u <ijh4u1@naver.com>
2023-06-05 23:31:56 +0900

    FEAT: 결제 쿠폰 조회

--- src/main/java/com/highfive/refurmoa/pay/controller/PayController.java
+++ src/main/java/com/highfive/refurmoa/pay/controller/PayController.java
@@ -1,13 +1,23 @@
 package com.highfive.refurmoa.pay.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.highfive.refurmoa.entity.Member;
 import com.highfive.refurmoa.pay.dto.request.PayRequestDTO;
 import com.highfive.refurmoa.pay.dto.response.PayInfoResponseDTO;
 import com.highfive.refurmoa.pay.dto.response.UserInfoResponseDTO;
+import com.highfive.refurmoa.pay.dto.response.couponListDTO;
 import com.highfive.refurmoa.pay.service.PayServiceImpl;
 import com.highfive.refurmoa.prod.controller.ProductController;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,6 +44,11 @@ public class PayController {
     public void insertPay(@RequestBody PayRequestDTO payRequestDTO) {
         payService.insertPay(payRequestDTO);
         productController.updateProdState(payRequestDTO.getProduct_code());
+    }
+    //회원 쿠폰 정보 조회
+    @GetMapping("/coupon")
+    public Page<couponListDTO> couponList(@RequestParam("id") String id, Pageable pageable) {
+        return payService.couponList(id, pageable);
     }
 
 }
